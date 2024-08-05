76040853f2eeb1ab1d31858b86b8e863a5992998
qleoz12 <kintaroloop@gmail.com>
2024-08-05 18:32:58 -0500

    fix name to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/git_actions_reactjs/'
+  base:'/React-Encriptador-Oracle-Alura-GIT-ACTIONS/'
 })
